7da7503fbea8fcef29faeef0cc844597938b1d70
BruderJakub <jakub.pilecki@stud.kbw.ch>
2026-09-02 15:53:50 +0200

    feat: add search profile by ID

--- backend/main.js
+++ backend/main.js
@@ -27,6 +27,28 @@ app.get('/profile', (req, res) => {
   db.close();
 });
 
+app.get('/profile/:profileID', (req, res) => {
+  const { profileID } = req.params;
+  const db = new sqlite3.Database(path.join(__dirname, '..', 'db', 'db.sqlite'), (err) => {
+    if (err) {
+      console.error('Error opening database:', err.message);
+      res.status(500).json({ error: 'Failed to open database' });
+    }
+  });
+
+  db.all('SELECT * FROM profile ORDER BY ROWID ASC LIMIT ?', [profileID], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json({
+        profile: rows
+      });
+    }
+  });
+
+  db.close();
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
