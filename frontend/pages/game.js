00ba22e2cc709622f0e5dd18d64514d7bdd48574
Bandnet <andre.bernet@stud.kbw.ch>
2026-09-11 15:11:38 +0200

    feat:display stats of selected player in gamepage

---
+++ frontend/pages/game.js
@@ -0,0 +1,32 @@
+let profileId = localStorage.getItem("profile");
+document.getElementById("selectedProfileId").textContent = profileId;
+
+async function getSingleProfileData(profileID) {
+    try {
+        const response = await fetch(`/profile/${profileID}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const profileData = document.getElementById(`profile-data`);
+        profileData.replaceChildren();
+        
+        data.profile.forEach((profile) => {
+            const profileElement = document.createElement('p');
+            profileElement.textContent = `${profile.name} - Level ${profile.level} - Money: ${profile.money}`;
+            profileData.appendChild(profileElement);
+        });
+        console.log(data);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+async function updateProfileData() {
+    getSingleProfileData(1);
+    getSingleProfileData(2);
+    getSingleProfileData(3);
+}
+
+// Call the function when the page loads
+window.onload = updateProfileData;
